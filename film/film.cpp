f31d84251765525dfcee2b3240d5c868c5036893
andead422 <andy.kuleshov@gmail.com>
2022-11-22 01:49:47 +0200

    222

--- film/film.cpp
+++ film/film.cpp
@@ -1,9 +1,27 @@
 #include "film.hpp"
 
-int FilmViewer::rateFilm(){
+Film& Film::operator =(const Film& film) {
+    idFilm = film.idFilm;
+    titleFilm = film.titleFilm;
+    genre = film.genre;
+    rate = film.rate;
+    
+    return *this;
+}
+
+int FilmViewer::rateFilm(int idFilm) {
     //parilka
 }
 
-int FilmAdmin::rateFilm(){
-    //parilka
+int FilmAdmin::rateFilm(int idFilm) {
+    cout << "rate film";
+
+    return 0;
+}
+
+FilmViewer& FilmViewer::operator =(const FilmViewer& filmViewer) {
+    Film::operator =(filmViewer);
+    imdbId = filmViewer.imdbId;
+
+    return *this;
 }
--- includes.hpp
+++ includes.hpp
@@ -1,7 +1,10 @@
 //est naitie chto ano dapamoget
-
+#include <iostream>
 #include <vector>
 #include <string>
 
 using std::vector;
 using std::string;
+using std::cin;
+using std::cout;
+using std::endl;
