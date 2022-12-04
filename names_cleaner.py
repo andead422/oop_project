67100478afc8196ee072732b3960fba5c6971d56
andead422 <andy.kuleshov@gmail.com>
2022-12-04 04:46:16 +0200

    names cleaned from dataset and last version of tags.csv

--- names_cleaner.py
+++ names_cleaner.py
@@ -1,41 +1,64 @@
-import csv
+import pymysql
 import tmdbsimple as tmdb
 import secret
 from fuzzywuzzy import fuzz
-from tempfile import NamedTemporaryFile
-import shutil
+import time
 
+
+conn = pymysql.connect(user=secret.getuser(), password=secret.getpass(), database=secret.getdb())
+cur = conn.cursor()
 tmdb.API_KEY = secret.getapikey()
-temp_file = NamedTemporaryFile(mode='w', delete=False)
 people = set()
 
 
-def delete_people():
+def person_checker(name):
+    search = tmdb.Search()
+    for res in search.person(query=name)['results']:
+        if fuzz.token_sort_ratio(res['name'], name) >= 95:
+            print('person: ' + name + ', ' + res['name'])
+            people.add(name.lower())
+            return 1
+    else:
+        print('not person: ' + name)
+        return 0
+
+
+def delete_people_new_version():
     global people
-    with open('ml-25m/tags.csv', 'r') as tags_file, temp_file:
-        reader = csv.reader(tags_file, delimiter=',')
-        writer = csv.writer(temp_file, delimiter=',')
-        writer.writerow('userId,movieId,tag')
-        next(reader)
-        for row_tags in reader:
-            if row_tags[2].lower() not in people:
-                if len(row_tags[2].split(' ')) >= 2:
-                    search = tmdb.Search()
-                    for res in search.person(query=row_tags[2])['results']:
-                        if fuzz.token_sort_ratio(res['name'], row_tags[2]) == 100:
-                            print('person: ' + row_tags[2] + ', ' + res['name'])
-                            people.add(row_tags[2].lower())
-                            break
-                    else:
-                        print('not person: ' + row_tags[2])
-                        writer.writerow(row_tags[0] + ',' + row_tags[1] + ',' + row_tags[2])
-                else:
-                    print('not person (too short): ' + row_tags[2])
-                    writer.writerow(row_tags[0] + ',' + row_tags[1] + ',' + row_tags[2])
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '%?%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '0%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '1%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '2%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '3%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '4%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '5%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '6%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '7%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '8%'")
+    cur.execute("DELETE FROM tag_tmp WHERE tag LIKE '9%'")
+    conn.commit()
+    cur.execute("SELECT DISTINCT tag FROM tag_tmp;")
+    tags = cur.fetchall()
+    for tag in tags:
+        if 2 <= len(tag[0].split(' ')) <= 5:
+            flag = 2
+            while flag > 0:
+                try:
+                    if person_checker(tag[0]):
+                        cur.execute(f"DELETE FROM tag_tmp WHERE tag LIKE '%{tag[0]}%'")
+                        conn.commit()
+                    break
+                except:
+                    time.sleep(2)
+                    print('reconnecting...')
+                    flag -= 1
             else:
-                print('person (in set): ' + row_tags[2])
-
-    shutil.move(temp_file.name, 'ml-25m/tags_3.csv')
+                print('not person (server not available): ' + tag[0])
 
 
-delete_people()
+delete_people_new_version()
+
+print(people)
+
+conn.commit()
+cur.close()
