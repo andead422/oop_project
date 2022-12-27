5fb7dbea0e28498359c8bd370d4bff938462e650
andead422 <andy.kuleshov@gmail.com>
2022-12-27 15:55:39 +0200

    test

--- dataset_sort.py
+++ dataset_sort.py
@@ -284,6 +284,10 @@ def add_tag():
                     f"INSERT INTO film_tag (id_film, id_tag, quantity) VALUES ({film_dict[film_tag]}, {tag_dict[tag]}, {len(word_tags[tag][film_tag])})")
 
 
+def test():
+    print(11111)
+
+
 def add_cast(partitions_quan=1):
     actors = set()
     directors = set()
