e734b5a9aff0c7e3cf2fc78ef5d01946c7d7ff01
nuinashco <havlytskyi.ivan@lll.kpi.ua>
2022-12-29 15:24:19 +0200

    da

--- main.cpp
+++ main.cpp
@@ -8,9 +8,67 @@ DBConnect database;
 
 int main()
 {
-    // DBConnect database;
-    Date dt;
+    //-----------------------------------------------------------------------------------------Date
+    cout << "------------------------------DATE------------------------------" << endl;
+    Date dt, test(23, 12, 2003);
+
+    cout << "Date(): **using printDate()**" << endl;
     dt.printDate();
     cout << endl;
-    cout << FilmViewer::getFilmsNumber();
+
+    cout << "Date(int, int, int): **using cout<<**" << endl;
+    cout << test << endl;
+
+    cin >> test;
+    cout << "cin >>: " << endl;
+    cout << test << endl;
+
+    Date dt1(test);
+    cout << "Date(const Date&):" << endl;
+    cout << dt1 << endl;
+
+    bool a = dt == dt1;
+    cout << a << endl;
+
+    bool b = dt > dt1;
+    cout << b << endl;
+
+    ++dt;
+    dt.printDate();
+    cout << endl;
+
+    dt++;
+    dt.printDate();
+    cout << endl;
+
+    b = dt != dt1;
+    cout << b << endl;
+
+    a = dt < dt1;
+    cout << a << endl;
+    /*
+    dt1 = test + dt;
+    dt1.printDate();
+    cout << endl;
+    */
+
+
+    //-----------------------------------------------------------------------------------------FilmViewer
+
+
+
+    //-----------------------------------------------------------------------------------------FilmAdmin
+
+
+
+    //-----------------------------------------------------------------------------------------Admin
+
+
+
+    //-----------------------------------------------------------------------------------------Viewer
+
+    
+
+    
+
 }
